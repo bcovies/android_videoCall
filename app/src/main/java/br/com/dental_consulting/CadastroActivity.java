29bb1d6597fa72622d1e4ce3f67817c8f548f70b
Bruno Souto <bcovies@gmail.com>
2021-11-25 20:52:16 -0300

    Adicionado restrições de cadastro

--- app/src/main/java/br/com/dental_consulting/CadastroActivity.java
+++ app/src/main/java/br/com/dental_consulting/CadastroActivity.java
@@ -71,7 +71,15 @@ public class CadastroActivity extends AppCompatActivity {
 
     private boolean validarNascimento(String nascimento){
         if(nascimento.isEmpty()){
-            Toast.makeText(CadastroActivity.this, "Nascimento: " + nascimento, Toast.LENGTH_SHORT).show();
+            Toast.makeText(CadastroActivity.this, "Nascimento nulo", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!nascimento.contains("/")){
+            Toast.makeText(CadastroActivity.this, "Nascimento está faltando / (01/01/1990)", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(nascimento.length() != 10){
+            Toast.makeText(CadastroActivity.this, "Nascimento deve ter 10 digitos (01/01/1990)", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
@@ -79,7 +87,11 @@ public class CadastroActivity extends AppCompatActivity {
 
     private boolean validarTelefone(String telefone){
         if(telefone.isEmpty()){
-            Toast.makeText(CadastroActivity.this, "Telefone: " + telefone, Toast.LENGTH_SHORT).show();
+            Toast.makeText(CadastroActivity.this, "Telefone nulo", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(telefone.length() != 11){
+            Toast.makeText(CadastroActivity.this, "Telefone deve ter 11 caracteres ex: 21999999999", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
@@ -87,8 +99,7 @@ public class CadastroActivity extends AppCompatActivity {
 
     private boolean validarNome(String nome){
         if(nome.isEmpty()){
-            Toast.makeText(CadastroActivity.this, "Nome: " + nome, Toast.LENGTH_SHORT).show();
-
+            Toast.makeText(CadastroActivity.this, "Nome nulo", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
@@ -96,9 +107,15 @@ public class CadastroActivity extends AppCompatActivity {
 
     private boolean validarSenha(String senha){
         if(senha.isEmpty()){
+            Toast.makeText(CadastroActivity.this, "Senha nula", Toast.LENGTH_SHORT).show();
             return false;
         }
         if(senha.length()<= 5){
+            Toast.makeText(CadastroActivity.this, "Senha tem que ter pelo menos 6 caracteres", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(senha.length()>16){
+            Toast.makeText(CadastroActivity.this, "Senha tem que ter no máximo 16 caracteres", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
@@ -106,9 +123,11 @@ public class CadastroActivity extends AppCompatActivity {
 
     private boolean validarEmail(String email){
         if(email.isEmpty()){
+            Toast.makeText(CadastroActivity.this, "Email nulo", Toast.LENGTH_SHORT).show();
             return false;
         }
         if(!email.contains("@")){
+            Toast.makeText(CadastroActivity.this, "Email deve conter @", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
