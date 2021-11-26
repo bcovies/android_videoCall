acd8cb5ed17f0ecdc77356ebac4468c98b14e327
Bruno Souto <bcovies@gmail.com>
2021-11-25 23:06:34 -0300

    Dashboard atualizado

--- app/src/main/java/br/com/dental_consulting/DashboardActivity.java
+++ app/src/main/java/br/com/dental_consulting/DashboardActivity.java
@@ -15,51 +15,22 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardActivity extends AppCompatActivity {
-    //Variáveis:
-    private FirebaseAuth firebaseAuth;
-    private TextView textView_userId;
-    private TextView textView_userEmail;
-    private Button button_resetEmail;
-    private Button button_call;
-    private FloatingActionButton floatingActionButton;
 
+    private FirebaseAuth mAuth;
+    private TextView dashboard_idUsuario;
+    private TextView dashboard_emailUsuario;
+    private Button botaoSair;
 
-    //Função para usuário sair do estado de logado;
-    private void usuarioSair() {
-        System.out.println("\nUsuarioSair:\nUsuário saindo...");
-        firebaseAuth.getInstance().signOut();
-    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth = FirebaseAuth.getInstance();
 
-    //Função para retornar email do usuário;
-    private String usuarioEmail() {
-        String email = firebaseAuth.getInstance().getCurrentUser().getEmail();
-        return email;
-    }
-    //Envia email de recuperação para o email registrado no firebaseAuth
-    private void resetEmailUsuario(){
-        firebaseAuth.getInstance().sendPasswordResetEmail(usuarioEmail()).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                System.out.println("Enviado email de recuperação!");
-            }
-        });
-    }
+        dashboard_idUsuario = findViewById(R.id.dashboard_textView_idUsuario);
+        dashboard_emailUsuario = findViewById(R.id.dashboard_textView_emailUsuario);
 
-    //Finaliza o activity atual e redireciona para o Login
-    public void acessaLoginActivity() {
-        finish();
-        startActivity(new Intent(this, MainActivity.class));
-    }
-
-    //Função para inicializar variáveis para onCrate;
-    private void inicializarVariaveis() {
-        textView_userId = findViewById(R.id.activity_dashboard_textView_userId);
-        textView_userEmail = findViewById(R.id.activity_dashboard_textView_userEmail);
-    }
-
-    private void setTextoVariaveis() {
-        textView_userId.setText(" Usuário UID: " + firebaseAuth.getInstance().getCurrentUser().getUid());
-        textView_userEmail.setText(" Usuário Email: " + firebaseAuth.getInstance().getCurrentUser().getEmail().toString());
+        dashboard_idUsuario.setText("Usuário UID: " + mAuth.getCurrentUser().getUid());
+        dashboard_emailUsuario.setText("Usuário Email: " + mAuth.getCurrentUser().getEmail());
     }
 
     @Override
@@ -67,42 +38,18 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        floatingActionButton = findViewById(R.id.activity_dashboard_fab);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        botaoSair = findViewById(R.id.dashboard_botao_sair);
+        botaoSair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 usuarioSair();
-                acessaLoginActivity();
             }
         });
-
-        button_resetEmail = findViewById(R.id.activity_dashboard_button_resetEmail);
-        button_resetEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetEmailUsuario();
-            }
-        });
-        button_call = findViewById(R.id.activity_dashboard_button_callActiviry);
-
-//        button_call.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                acessaCallActivity();
-//            }
-//        });
-
     }
 
-//    public void acessaCallActivity() {
-//        finish();
-//        startActivity(new Intent(this, CallActivity.class));
-//    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        inicializarVariaveis();
-        setTextoVariaveis();
+    public void usuarioSair() {
+        mAuth.signOut();
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
