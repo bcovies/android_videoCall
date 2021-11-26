c2f2529cdc6d2750d83e5e69af3226cb702fd076
Bruno Souto <bcovies@gmail.com>
2021-11-26 12:56:24 -0300

    wip: pesquisa de usuário por email

--- app/src/main/java/br/com/dental_consulting/ChamadaActivity.java
+++ app/src/main/java/br/com/dental_consulting/ChamadaActivity.java
@@ -1,5 +1,6 @@
 package br.com.dental_consulting;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,15 +19,25 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.UUID;
 
+import br.com.dental_consulting.models.Usuario;
+
 public class ChamadaActivity extends AppCompatActivity {
-    private static boolean isPeerConnected;
+
+    private static boolean isPeerConnected = false;
     private String username;
     private String emailDestinatario;
-//    private boolean isPeerConnected = false;
     private boolean isAudio = false;
     private boolean isVideo = false;
     private String uniqueId = "";
@@ -37,7 +48,16 @@ public class ChamadaActivity extends AppCompatActivity {
     private ImageView botao_audio;
     private ImageView botao_video;
 
-    private DatabaseReference mReference;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +73,7 @@ public class ChamadaActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 emailDestinatario = chamar_email.getText().toString();
-                sendCallRequest();
+                sendCallRequest(emailDestinatario);
             }
         });
 
@@ -89,10 +109,44 @@ public class ChamadaActivity extends AppCompatActivity {
         setupWebView();
     }
 
-    private void sendCallRequest(){
-        if (!isPeerConnected) {
+    private void sendCallRequest(String email){
+        if (isPeerConnected) {
             Toast.makeText(this, "You're not connected. Check your internet", Toast.LENGTH_LONG).show();
         }else{
+            System.out.println(email);
+            DatabaseReference procurarEmail = mDatabase.child("usuarios");
+            Query query = procurarEmail.child("dados").orderByChild("email").equalTo(email);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    System.out.println("Email existe!!");
+
+                    if(snapshot.exists()){
+                        System.out.println("Email existe!!");
+
+                        System.out.println(snapshot.getValue());
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    System.out.println(error);
+                }
+            });
+            System.out.println( mDatabase.equalTo(email).toString());
+            System.out.println("fim");
+//            Usuario usuario = new Usuario();
+//            mDatabase.child("usuarios").child("ONeyiZuLvDZJhurBYR7GrvEOraZ2").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+//                @Override
+//                public void onComplete(@NonNull Task<DataSnapshot> task) {
+//                    if (!task.isSuccessful()) {
+//                        System.out.println("firebase: Error getting data: " + task.getException());
+//                    }
+//                    else {
+//                       System.out.println("firebase: "+ String.valueOf(task.getResult().getValue()));
+//                    }
+//                }
+//            });
             // faz uma busca no firebase para saber se o usuário está disponível,
             // caso esteja, muda o campo do destinatário para o usuário remetente do chamado chama
             // listenForConnId()
