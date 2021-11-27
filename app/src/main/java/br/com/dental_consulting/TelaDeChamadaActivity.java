10802222226277ebff9fbf6bca31607318ce420c
Bruno Souto <bcovies@gmail.com>
2021-11-27 14:44:01 -0300

    Adicionado mecanica de chamada

---
+++ app/src/main/java/br/com/dental_consulting/TelaDeChamadaActivity.java
@@ -0,0 +1,14 @@
+package br.com.dental_consulting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaDeChamadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_chamada);
+    }
+}
